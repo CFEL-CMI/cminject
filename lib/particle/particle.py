869ffad144c23e764fc037e45a19e76233b86cef
Muhamed Amin <aminmuha@CFELD-PCX15768.desy.de>
2017-09-20 18:02:18 +0200

    spherical particle

--- lib/particle/particle.py
+++ lib/particle/particle.py
@@ -1,3 +1,13 @@
-class particle:
+class Particle:
+   def __init__(self, temp, mass, density, thermal_conductivity):
+     self.temp = temp
+
+
+class SphericalParticle(Particle):
   """This class define the characteristics of the particles that will be added to the simulation"""
-  def __init__(self, shape, size, index_of_ref):
+  def __init__(self, radius, temp, density, thermal_conductivity, index_of_ref ):
+    self.radius = radius
+    self.temp = temp
+    self.density = density
+    self.thermal_conductivity = thermal_conductivity 
+    self.index_of_ref =index_of_ref
