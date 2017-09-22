d3e2e71df03518e1b9673fc55f972096f980f22a
Muhamed Amin <aminmuha@CFELD-PCX15768.desy.de>
2017-09-22 10:37:30 +0200

    adjusted experiment class

--- lib/device/experiment.py
+++ lib/device/experiment.py
@@ -1,10 +1,19 @@
-class experiment:
+class Experiment:
   """This class carry out the info about the experiment you want to simulate. 
      For example, the devices and the interaction fields you will include.
      The output files will have the name of and date of your experiment""" 
 
-  def __init__(self, name, date, devices, fields=None):
+  def __init__(self, name, date, detector, source, adl=None, field=None):
     self.name = name
     self.date = date
-    self.devices = devices
-    self.field = field
+    self.field = []
+    self.device = []
+
+  def add_device(self, device):
+    self.device.append(device)
+
+  def add_field(self, device):
+    self.field.append(field)
+
+  def run(self):
+   pass
