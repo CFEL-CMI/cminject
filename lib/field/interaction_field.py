036b1116f1468f7f87b49f369a86467a3910519b
Muhamed Amin <aminmuha@CFELD-PCX15768.desy.de>
2017-09-26 13:22:53 +0200

    reading interaction from file

--- lib/field/interaction_field.py
+++ lib/field/interaction_field.py
@@ -8,21 +8,23 @@ class interaction_field:
 
 class EM(interaction_field):
   """If the interaction field is EM field then the set_method function here will be used"""
-   def __init__(self, intensity, energy):
+  def __init__(self, intensity, energy):
      self.intensity = intensity
      self.energy = energy
 
-   def set_method(self, method='imperical'):
+  def set_method(self, method='imperical'):
      self.method = method
   
 
 
-class fluid(interaction_field):
-  """If the interaction field is EM field then the set_method function here will be used"""
-   def __init__(self, density, kinematic_viscosity, inflow_velocity):
+class Fluid(interaction_field):
+   """If the interaction field is EM field then the set_method function here will be used"""
+   def __init__(self, density, kinematic_viscosity, inflow_speed, outflow_pressure, method='LBM'):
      self.density = density
      self.kinematic_viscosity = kinematic_viscosity
-     self.inflow_velocity = inflow_velocity
+     self.inflow_speed =inflow_speed
+     self.outflow_pressure = outflow_pressure
+     self.method = method
 
    def set_method(self, method='LB'):
      self.method = method
@@ -31,5 +33,31 @@ class fluid(interaction_field):
      self.inflow_v = inflow_v
      self.outflow_p = outflow_p
 
-
+   def ReadFromFile(self, filename):
+     rawdata = open(filename,'r')
+     # Read the file contents and generate a list with each line
+     lines = rawdata.readlines()
+     rawdata.close()
+     #define variables
+     rField=[]
+     zField=[]
+     vrField=[]
+     vzField=[]
+     pField=[]
+     #write the lines into variables
+     for line in lines:
+        #remove eol
+        newline=line[:-1]
+        linesplit=newline.split(";")
+        rField.append(float(linesplit[0]))
+        zField.append(float(linesplit[1]))
+        vrField.append(float(linesplit[2]))
+        vzField.append(float(linesplit[3]))
+        pField.append(float(linesplit[4]))
+     rField=np.array(rField)
+     zField=np.array(zField)
+     vrField=np.array(vrField)
+     vzField=np.array(vzField)
+     pField=np.array(pField)
+     return rField, zField, vrField, vzField, pField
     
