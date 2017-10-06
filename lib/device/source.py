7e575cde2083f70399f4d601db08cdfac8591856
Muhamed Amin <muhamed.amin@desy.de>
2017-10-06 13:00:32 +0200

    small edit, give the source location instead muP

--- lib/device/source.py
+++ lib/device/source.py
@@ -6,12 +6,12 @@ particle = imp.load_source('particle', '/Users/aminmuha/Documents/myproject/cmi-
 import numpy as np
 
 class Source:
-  def __init__(self, number_of_particles, muP, sigmaP, muV, sigmaV, distribution='gaussian'):
+  def __init__(self, number_of_particles, position, sigmaP, muV, sigmaV, distribution='gaussian'):
     self.particles = [] 
     self.number_of_particles = number_of_particles
     self.sigmaP = sigmaP
     self.sigmaV = sigmaV 
-    self.muP = muP
+    self.muP = position[0]
     self.muV = muV
     self.distribution = distribution
     self.generate_particles()
@@ -21,10 +21,10 @@ class Source:
     velocity = np.random.normal(self.muV, self.sigmaV, self.number_of_particles)
     for i in range(self.number_of_particles):
       self.particles.append(particle.SphericalParticle(0.00001, 0, 
-           0, 0, 0, None, position=(position[i],0,0), velocity=(velocity[i],0,0)))
+           0, 0, 0, None, position=(0, position[i]+0.01, 0), velocity=(velocity[i],0,0)))
 
 
 
-p = Source( 1000, 0, 3, 10, 5 )
+p = Source( 1000, (0, 0.01) , 0.001, 10, 5 )
 for i in p.particles:
   print i.velocity, i. position
