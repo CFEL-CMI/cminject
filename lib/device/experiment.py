50c4da2fbfb3f543521a19438eac965b76d6b642
Muhamed Amin <muhamed.amin@desy.de>
2017-10-07 17:18:36 +0200

    first functional simple example

--- lib/device/experiment.py
+++ lib/device/experiment.py
@@ -1,13 +1,32 @@
+from scipy.integrate import ode
+
 class Experiment:
   """This class carry out the info about the experiment you want to simulate. 
      For example, the devices and the interaction fields you will include.
      The output files will have the name of and date of your experiment""" 
 
-  def __init__(self, name, date, detector, source, adl=None, field=None):
+  def __init__(self, name, date, source, detector=None, adl=None, field=None):
     self.name = name
     self.date = date
     self.field = []
     self.device = []
+    self.source = source
+    self.CalculateTrajectory()
+
+  def CalculateTrajectory(self, tStart=0, tEnd=2 ):
+    """ Calculate the particle trajectories by integrating the equation of motion"""
+    count = 0
+    for i in self.source.particles:
+      integral = ode( i.get_v_and_a )
+      integral.set_integrator('vode',method='BDF',with_jacobian=False,atol=1e-6,rtol=1e-6,first_step=1e-5,nsteps=1000)
+      integral.set_initial_value( i.position + i.velocity, tStart )
+      print "Calculating particle: ", count
+      while integral.successful() and integral.t < tEnd:
+        i.trajectory.append( (integral.y[0], integral.y[1], integral.y[2]) )
+        integral.integrate(integral.t + 0.00001)
+      count+=1
+    return integral
+    
 
   def add_device(self, device):
     self.device.append(device)
