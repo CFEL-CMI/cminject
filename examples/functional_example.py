8a6f5641ee8df9f980dac6ce04a7a7d5eeb1425b
Muhamed Amin <muhamed.amin@desy.de>
2017-10-09 21:17:24 +0200

    3d visualizer

--- examples/functional_example.py
+++ examples/functional_example.py
@@ -3,11 +3,11 @@ sys.path.insert(0, '../lib')
 
 from device.source import *
 from device.experiment import *
-from visualizer.visualizer import *
+from visualizer.visualizer3d import *
 import matplotlib.pyplot as plt
 
 
-NumberOfParticles = 10
+NumberOfParticles = 5
 SourceCoordinates = (0, 0.01)
 SigmaParticlesPosition = 0.003
 MuParticlesVelocity = 10
