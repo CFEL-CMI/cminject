996206e08408203e7ea821d9687ace08cfe2c52d
Muhamed Amin <muhamed.amin@desy.de>
2017-10-13 21:28:19 +0200

    edited asl

---
+++ lib/device/als.py
@@ -0,0 +1,17 @@
+class AerodynamicsLensStack:
+    """ This class implement the aerodynamic lens stack. It is contains a several adl segments"""
+    def __init__(self, position, segments):
+        self.position = position
+        self.segments = segments
+    
+    def add_fluid(self, fluid):
+        self.fluid = fluid
+    
+    def set_boundary_condition(self, inflow, outflow):
+        self.inflow = inflow
+        self.outflow = outflow
+        return
+
+    def add_particle(self, particle):
+        return particle
+
