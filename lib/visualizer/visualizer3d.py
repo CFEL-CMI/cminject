2f943875323a176d0bc8696dc3d88610c5ed26e0
Muhamed Amin <muhamed.amin@desy.de>
2017-10-13 21:26:50 +0200

    draw any als

--- lib/visualizer/visualizer3d.py
+++ lib/visualizer/visualizer3d.py
@@ -1,4 +1,6 @@
 import numpy as np
+#import matplotlib
+#matplotlib.use("Agg")
 from mpl_toolkits.mplot3d import Axes3D 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -6,7 +8,7 @@ import matplotlib.animation as animation
 class visualizer:
   def __init__(self, exp):
     self.exp = exp
-    self.n = len(self.exp.source.particles[0].trajectory)
+    self.n = exp.LongestTrajectory()
     self.n_particles = self.exp.source.particles
     self.plot()
 
@@ -15,21 +17,22 @@ class visualizer:
         datay=[]
         dataz=[]
         for j in self.n_particles:
+         if len(j.trajectory)>i:
            datax.append(j.trajectory[i][0])
            datay.append(j.trajectory[i][1])
            dataz.append(j.trajectory[i][2])
         return datax, datay, dataz
 
   def plot_device(self, ax):
-    r1 = [ (-0.01, 0.01), (-0.001, 0.001), (-0.01, 0.01), (-0.001, 0.001)]
-    z1 = [ ( 0.0, -0.025), (-0.025, -0.030), (-0.03, -0.045), (-0.045, -0.05)] 
 
-    for i,j in zip(r1,z1):
-      x = np.linspace(i[0], i[1], 100)
-      z = np.linspace(j[0], j[1], 100)
+    offset = self.exp.als.position[2]
+    for i in self.exp.als.segments:
+      x = np.linspace(self.exp.als.position[0]+i[0], self.exp.als.position[0]-i[0], 100)
+      z = np.linspace(offset, offset+i[1], 100)
       Xc, Zc=np.meshgrid(x, z)
       Yc = np.sqrt(i[0]*i[0]-Xc**2)
-
+      offset = offset + i[1]
+      
       # Draw parameters
       rstride = 20
       cstride = 10
@@ -46,11 +49,21 @@ class visualizer:
     ax.set_ylim(-0.01, 0.01)
     ax.set_ylabel("Y")
     ax.set_zlim(0, -0.05)
+
+
+
+#    FFMpegWriter = animation.writers['ffmpeg']
+#    metadata = dict(title='Movie Test', artist='Matplotlib',
+#                comment='Movie support!')
+#    writer = FFMpegWriter(fps=15, metadata=metadata)
+#    with writer.saving(fig, "writer_test.mp4", 100):
     for i in range(self.n):
-     if plt.fignum_exists(fig.number):
-      x, y, z = self.data_gen(i)
-      p = ax.plot(x, y, z, 'o-', lw=0)
-      plt.pause(0.01)
-      p.pop(0).remove()
-     else:
-       plt.close()
+       if plt.fignum_exists(fig.number):
+         x, y, z = self.data_gen(i)
+         p = ax.plot(x, y, z, 'o-', lw=0)
+#         writer.grab_frame()
+         plt.pause(0.01)
+         p.pop(0).remove()
+   
+       else:
+         plt.close()
