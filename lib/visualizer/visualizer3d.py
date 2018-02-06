8d831b8bf374116e60121d90086ff8f6a90e718f
Muhamed Amin <muhamed.amin@desy.de>
2018-02-06 12:54:01 +0100

    improve 3d visualization

--- lib/visualizer/visualizer3d.py
+++ lib/visualizer/visualizer3d.py
@@ -1,4 +1,5 @@
 from device.als import *
+from device.bgc import *
 import numpy as np
 #import matplotlib
 #matplotlib.use("Agg")
@@ -27,6 +28,7 @@ class visualizer:
   def plot_device(self, ax):
     for i in self.exp.devices:
       if type(i) is AerodynamicsLensStack:
+        print "Plot ALS"
         offset = i.position[2]
         for j in i.segments:
           x = np.linspace(i.position[0]+j[0], i.position[0]-j[0], 100)
@@ -38,21 +40,47 @@ class visualizer:
       # Draw parameters
           rstride = 20
           cstride = 10
+          ax.set_xlim(-0.01, 0.01)
+          ax.set_xlabel("X")
+          ax.set_ylim(-0.01, 0.01)
+          ax.set_ylabel("Y")
+          ax.set_zlim(0, -0.05)
+
+
           ax.plot_surface(Xc, Yc, Zc, alpha=0.2, rstride=rstride, cstride=cstride)
           ax.plot_surface(Xc, -Yc, Zc, alpha=0.2, rstride=rstride, cstride=cstride)
+          ax.view_init(elev=5., azim=-55)
 
+      if type(i) is BufferGasCell:
+        yvec = np.arange(0., 0.035, 0.00005)
+        zvec = np.arange(0., 0.035, 0.00005)
+        Y, Z = np.meshgrid(yvec, zvec)
+        X = np.sqrt((Z-0.015)**2 + (Y-0.015)**2)-0.015**2
+        X[X < 0.002] = np.nan
+        X[X > 0.012] = np.nan
+        X1 = -(X-0.042)
+        rstride = 20
+        cstride = 10
+        Xc = np.sqrt(0.015**2-(Y-0.015)**2) +0.015
+        #ax.plot_wireframe(X, Y, Z)
+        ax.plot_surface(X, Y, Z, alpha=0.2, rstride=rstride, cstride=cstride)
+        ax.plot_surface(X1, Y, Z, alpha=0.2, rstride=rstride, cstride=cstride)
+        Z[Z<0.012]=np.nan
+        Z[Z>0.032]=np.nan
+        ax.plot_surface(Z, Y, Xc, alpha=0.2, rstride=rstride, cstride=cstride)
+        ax.plot_surface(Z, Y, 0.03-Xc, alpha=0.2, rstride=rstride, cstride=cstride)
+        ax.view_init(elev=5., azim=-90)
+       # ax.set_zlim(0,2)
+        ax.set_xlim(-0.0, 0.045)
+        ax.set_xlabel("X")
+        ax.set_ylim(-0.0, 0.030)
+        ax.set_ylabel("Y")
+        ax.set_zlim(0, 0.03)
 
   def plot(self):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.view_init(elev=5., azim=-55)
     self.plot_device(ax)
-    ax.set_xlim(-0.01, 0.01)
-    ax.set_xlabel("X")
-    ax.set_ylim(-0.01, 0.01)
-    ax.set_ylabel("Y")
-    ax.set_zlim(0, -0.05)
-
 
 
 #    FFMpegWriter = animation.writers['ffmpeg']
