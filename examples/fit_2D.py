a1813787b979e1e7e012d831f57db78205527aa2
Daniel Horke <daniel.horke@cfel.de>
2018-07-04 11:01:09 +0200

    fixed errors with fit_2D for mac

--- examples/fit_2D.py
+++ examples/fit_2D.py
@@ -25,11 +25,12 @@ def filter(x, y, vx, vy, vz, ID, X, Y, VX, VY, VZ):
   if x is not None and y is not None:
     mask = np.zeros(len(x), dtype=bool)
     for i in range(len(ID)):
-      if (x[ID[i]]>X[0] and x[ID[i]]<X[1]
-            and y[ID[i]]>Y[0] and y[ID[i]]<Y[1]
-              and vx[ID[i]]>VX[0] and vx[ID[i]]<VX[1]
-                and vy[ID[i]]>VY[0] and vy[ID[i]]<VY[1]
-                  and vz[ID[i]]>VZ[0] and vz[ID[i]]<VZ[1]):
+      print(int(ID[i]))
+      if (x[int(ID[i])]>X[0] and x[int(ID[i])]<X[1]
+            and y[int(ID[i])]>Y[0] and y[int(ID[i])]<Y[1]
+              and vx[int(ID[i])]>VX[0] and vx[int(ID[i])]<VX[1]
+                and vy[int(ID[i])]>VY[0] and vy[int(ID[i])]<VY[1]
+                  and vz[int(ID[i])]>VZ[0] and vz[int(ID[i])]<VZ[1]):
         mask[i] = True
     return mask
 
@@ -43,7 +44,7 @@ VZrange = (-100., 80.)
 
 x = None
 for i in range(3, len(sys.argv)):
-  f = h5py.File(sys.argv[i], 'r')  
+  f = h5py.File(sys.argv[i], 'r')
   d = f['detectors']
   s = f['source']['source']
   det = d[sys.argv[1]]
@@ -51,9 +52,9 @@ for i in range(3, len(sys.argv)):
   ys = s[1]
   vxs = s[3]
   vys = s[4]
-  vzs = s[5] 
+  vzs = s[5]
   ID = det[7]
-  mask = filter(xs, ys, vxs, vys, vzs, ID, 
+  mask = filter(xs, ys, vxs, vys, vzs, ID,
                Xrange, Yrange, VXrange, VYrange, VZrange)
 
   if sys.argv[2]=='p':
@@ -74,14 +75,14 @@ for i in range(3, len(sys.argv)):
 
 
 
-print "Number of particles: ", len(x)
+print ("Number of particles: ", len(x))
 histy = np.array(y)
 histx = np.array(x)
 (mux, sigmax) = norm.fit(histx)
 (muy, sigmay) = norm.fit(histy)
-print 'number of particles', len(x)
-print "======================Initial Guess================"
-print 'mean X',mux, 'mean Y', muy, 'sig X', sigmax, 'sig Y', sigmay
+print ('number of particles', len(x))
+print ("======================Initial Guess================")
+print ('mean X',mux, 'mean Y', muy, 'sig X', sigmax, 'sig Y', sigmay)
 
 n = 100
 #plt.hist2d(histx, histy, bins=n)
@@ -96,14 +97,14 @@ popt, pcov = optimize.curve_fit(Gaussian2D, (X, Y), H_rot.ravel(), p0=(initial_g
                initial_guess[1],initial_guess[2],initial_guess[3],initial_guess[4],initial_guess[5],
                  initial_guess[6]),maxfev=100000)
 
-print "===================Fitting Results================="
-print 'Amplitude', popt[0]
-print 'X center', popt[1]
-print 'Y center', popt[2]
-print 'Major FWHM', max([abs(popt[3]), abs(popt[4])])*2.355
-print 'Minor FWHM', min([abs(popt[3]), abs(popt[4])])*2.355
-print 'Angle', popt[5]
-print 'Offset', popt[6]
+print ("===================Fitting Results=================")
+print ('Amplitude', popt[0])
+print ('X center', popt[1])
+print ('Y center', popt[2])
+print ('Major FWHM', max([abs(popt[3]), abs(popt[4])])*2.355)
+print ('Minor FWHM', min([abs(popt[3]), abs(popt[4])])*2.355)
+print ('Angle', popt[5])
+print ('Offset', popt[6])
 
 data_fitted = Gaussian2D((X, Y), *popt)
 fig, ax = plt.subplots(1, 1)
@@ -113,4 +114,3 @@ ax.contour(X, Y, data_fitted.reshape(n-1,n-1), 8, colors='w')
 cbar = fig.colorbar(cax)
 plt.tight_layout()
 plt.show()
-
