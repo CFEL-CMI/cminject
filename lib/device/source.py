876b97f2601723b66b5f334263e01cac6bcadaaf
Muhamed Amin <aminmuha@max-cfel009.desy.de>
2018-10-15 19:23:43 +0200

    allowing the source to only generate one particle and will raise exception if zero

--- lib/device/source.py
+++ lib/device/source.py
@@ -20,14 +20,25 @@ class Source:
   def generate_particles(self):
     """ Given the parameters of the normal distribution this function generates the intial positions
         and momentum of the particles"""
+    if self.number_of_particles > 1:
+      x = np.random.normal(self.muP[0], self.sigmaP[0], self.number_of_particles)
+      y = np.random.normal(self.muP[1], self.sigmaP[1], self.number_of_particles) 
+      z = np.random.normal(self.muP[2], self.sigmaP[2], self.number_of_particles)
+      vx = np.random.normal(self.muV[0], self.sigmaV[0], self.number_of_particles)
+      vy = np.random.normal(self.muV[1], self.sigmaV[1], self.number_of_particles)
+      vz = np.random.normal(self.muV[2], self.sigmaV[2], self.number_of_particles)
 
-    x = np.random.normal(self.muP[0], self.sigmaP[0], self.number_of_particles)
-    y = np.random.normal(self.muP[1], self.sigmaP[1], self.number_of_particles) 
-    z = np.random.normal(self.muP[2], self.sigmaP[2], self.number_of_particles)
-    vx = np.random.normal(self.muV[0], self.sigmaV[0], self.number_of_particles)
-    vy = np.random.normal(self.muV[1], self.sigmaV[1], self.number_of_particles)
-    vz = np.random.normal(self.muV[2], self.sigmaV[2], self.number_of_particles)
-    
+    elif self.number_of_particles == 1:
+      x = [self.muP[0]]
+      y = [self.muP[1]]
+      z = [self.muP[2]]
+      vx = [self.muV[0]]
+      vy = [self.muV[1]]
+      vz = [self.muV[2]]
+
+    elif self.number_of_particles == 0:
+      raise ValueError('The number of particles cannot be zero')
+
     for i in range(self.number_of_particles): # create an object for each particle
       self.particles.append(particle.SphericalParticle(self.radius, self.rho, 
    			position=(x[i], y[i], z[i]), velocity=(vx[i],vy[i],vz[i])))
