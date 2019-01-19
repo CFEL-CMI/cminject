1466a6bf25d20efb74183fb28c084303e340c3cb
Muhamed Amin <aminmuha@max-cfel002.desy.de>
2019-01-19 13:15:23 +0100

    now the particles' masses are normal distribution ratherthan a single value

--- lib/cminject/experiment/source.py
+++ lib/cminject/experiment/source.py
@@ -4,7 +4,8 @@ import numpy as np
 class Source:
   """This class generate particles with normal distribution of their initial position and momentum"""
 
-  def __init__(self, number_of_particles, position, sigmaP, muV, sigmaV, radius=0.00001, rho=1, distribution='gaussian', seed=True):
+  def __init__(self, number_of_particles, position, sigmaP, muV, sigmaV, 
+               radius=1.e-6, sigma_radius=1.e-7, rho=1, distribution='gaussian', seed=None):
     self.particles = [] #multiprocessing.Manager().list() 
     self.number_of_particles = number_of_particles
     self.sigmaP = sigmaP
@@ -13,9 +14,10 @@ class Source:
     self.muV = muV
     self.distribution = distribution
     self.radius = radius
+    self.sigma_radius = sigma_radius
     self.rho = rho
-    if seed:
-      np.random.seed(1000)
+    if seed is not None:
+      np.random.seed(seed)
     self.generate_particles()
 
   def generate_particles(self):
@@ -29,6 +31,7 @@ class Source:
       vx = np.random.normal(self.muV[0], self.sigmaV[0], self.number_of_particles)
       vy = np.random.normal(self.muV[1], self.sigmaV[1], self.number_of_particles)
       vz = np.random.normal(self.muV[2], self.sigmaV[2], self.number_of_particles)
+      r = np.random.normal(self.radius, self.sigma_radius, self.number_of_particles)
     
     elif self.number_of_particles == 1:
       x = [self.muP[0]]
@@ -37,12 +40,12 @@ class Source:
       vx = [self.muV[0]]
       vy = [self.muV[1]]
       vz = [self.muV[2]]
-
+      r = [self.radius]
     elif self.number_of_particles == 0:
       raise ValueError('The number of particles cannot be zero')
 
     for i in range(self.number_of_particles): # create an object for each particle
-      self.particles.append(particle.SphericalParticle(i, self.radius, self.rho, 
+      self.particles.append(particle.SphericalParticle(i, r[i], self.rho, 
    			position=(x[i], y[i], z[i]), velocity=(vx[i],vy[i],vz[i])))
 
 
