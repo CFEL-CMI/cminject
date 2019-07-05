d756da313e4a49166fd41fa8f75473b81acfdd5a
Simon Welker <simon.welker@desy.de>
2019-07-05 22:24:47 +0200

    Add a linearly distributing source

--- lib/cminject/experiment_refactored/definitions/sources.py
+++ lib/cminject/experiment_refactored/definitions/sources.py
@@ -91,6 +91,81 @@ class GaussianDistributedSphericalSource(Source):
         return self.particles
 
 
+class LinearlyDistributedSphericalSource(Source):
+    def __init__(self,
+                 number_of_particles: int,
+                 position_intervals: List[Tuple[float, float]],
+                 velocity_intervals: List[Tuple[float, float]],
+                 radius_interval: Tuple[float, float],
+                 rho: float,
+                 seed=None,
+                 radially_symmetrical: bool = False,
+                 subclass: Type[SphericalParticle] = SphericalParticle,
+                 **subclass_kwargs):
+        self.particles = []
+        if number_of_particles < 1:
+            raise ValueError('The number of particles must be >= 1')
+
+        self.number_of_particles = number_of_particles
+        self.position_intervals = position_intervals
+        self.velocity_intervals = velocity_intervals
+        self.radius_interval = radius_interval
+        self.rho = rho
+        self.seed = seed
+        self.radially_symmetrical = radially_symmetrical
+        self.subclass = subclass
+        self.subclass_kwargs = subclass_kwargs
+
+        self.number_of_dimensions = len(self.position_intervals)
+
+    def set_number_of_dimensions(self, number_of_dimensions: int):
+        if number_of_dimensions != self.number_of_dimensions:
+            raise ValueError(
+                f"Incompatible number of dimensions: {number_of_dimensions}, "
+                f"the position description passed at construction was {self.number_of_dimensions}-dimensional."
+            )
+
+    def _rotate(self, positions):
+        size = positions.shape[1]
+        rotated = (positions[0] + positions[1]*1j) * np.exp(np.random.random(size) * 2 * np.pi * 1j)
+        return np.array([rotated.real, rotated.imag, positions[2]])
+
+    def generate_particles(self, start_time: float = 0.0):
+        """
+        Given the parameters of the normal distribution this function generates
+        the initial positions and momentum of the particles
+        """
+        if self.seed is not None:
+            np.random.seed(self.seed)
+
+        positions = np.array([
+            np.linspace(pi[0], pi[1], self.number_of_particles) for pi in self.position_intervals
+        ])
+        if self.number_of_dimensions == 3 and self.radially_symmetrical:
+            # Randomly rotate the positions around the Z axis for 3D radially symmetrical simulations
+            positions = self._rotate(positions)
+        positions = positions.transpose()
+
+        velocities = np.array([
+            np.linspace(vi[0], vi[1], self.number_of_particles) for vi in self.velocity_intervals
+        ]).transpose()
+        r = np.linspace(self.radius_interval[0], self.radius_interval[1], self.number_of_particles)
+
+        self.particles = []
+        for i in range(self.number_of_particles):
+            inst = self.subclass(
+                identifier=i,
+                position=np.concatenate([positions[i], velocities[i]]),
+                start_time=start_time,
+                radius=r[i],
+                rho=self.rho,
+                **self.subclass_kwargs
+            )
+            self.particles.append(inst)
+
+        return self.particles
+
+
 """
 ### Local Variables:
 ### fill-column: 100
