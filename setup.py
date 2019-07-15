569c5f81259a8ba79654bb520624399daea97158
Simon Welker <simon.welker@desy.de>
2019-07-15 16:53:15 +0200

    Update requirements, use nicer theme for Sphinx HTML

--- setup.py
+++ setup.py
@@ -11,8 +11,7 @@ import sys
 
 long_description = """CMI Injector -- Simulating particles' trajectories in different forcefields
 
-
-Developed by Muhamed Amin and the Controlled Molecule Imaging Group at the Center for
+Developed by Muhamed Amin, Simon Welker and the Controlled Molecule Imaging Group at the Center for
 Free-Electron Laser Science, Deutsches Elektronen-Synchrotron DESY and Universität Hamburg, Hamburg,
 Germany.
 
@@ -26,38 +25,32 @@ packages = find_packages(where="lib")
 
 provides = [
     'cminject',
+    'txt_to_hdf5',
 ]
-
-
-
-
-requires = [
-    'Python (>=3.0)',
-    'scipy (>=0.14.1)',
-    'numpy (>= 1.7.1)']
 
 install_requires = [
-    'Python>=3.0',
-    'scipy>=0.14.1',
-    'numpy>=1.7.1',
+    'scipy>=1.3.0',
+    'numpy>=1.16.0',
+    'pandas>=0.24.0',
+    'matplotlib>=3.1.0',
+    'h5py>=2.9.0',
+    'h5sparse>=0.1.0',
+    'sphinx_rtd_theme~=0.4.3'
 ]
 
-
-setup(name="cmi-inject",
-      author              = "Muhamed Amin and the CFEL-CMI group",
-      author_email        = "muhamed.amin@cfel.de",
-      maintainer          = "muhamed amin and the CFEL-CMI group",
-      maintainer_email    = "muhamed.amin@cfel.de",
-      url                 = "https://stash.desy.de/projects/CMIFLY/repos/cmi-injector/browse",
-      description         = "CMI inject",
-      version             = "0.0.0",
-      long_description    = long_description,
-      license             = "GPL",
-      package_dir         = package_dir,
-      packages            = packages,
-      scripts             = None,
-      requires            = requires,
-      provides            = provides,
-#      install_requires    = install_requires,
-      )
-
+setup(name="cminject",
+      author="Muhamed Amin, Simon Welker and the CFEL-CMI group",
+      author_email="muhamed.amin@cfel.de",
+      maintainer="Muhamed Amin and the CFEL-CMI group",
+      maintainer_email="muhamed.amin@cfel.de",
+      url="https://stash.desy.de/projects/CMIFLY/repos/cmi-injector/browse",
+      description="A framework for particle injection trajectory simulations in different force fields",
+      version="0.1.0",
+      long_description=long_description,
+      license="GPL",
+      package_dir=package_dir,
+      packages=packages,
+      scripts=None,
+      provides=provides,
+      python_requires='>=3.7',
+      install_requires=install_requires)
