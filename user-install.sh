ff40410a2f266732aa2cbf97eda996cb5ac0753a
Simon Welker <simon.welker@desy.de>
2019-08-14 11:16:14 +0200

    user_install.sh: Try importing Cython and numpy before installing

--- user-install.sh
+++ user-install.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
-python3 -m pip install --user Cython numpy  # install requirements for setup.py
+
+# Try importing cython before installing it
+python3 -c "import Cython; import cython"
+if [[ $? -ne 0 ]]; then
+    echo "Cython not found, installing to user dir..." >&2
+    python3 -m pip install --user Cython
+fi
+
+# Try importing numpy before installing it
+python3 -c "import numpy"
+if [[ $? -ne 0 ]]; then
+    echo "numpy not found, installing to user dir..." >&2
+    python3 -m pip install --user numpy
+fi
+
 python3 setup.py build_ext --inplace        # use setup.py to build extensions inplace
 python3 setup.py install --user             # install the software, which also installs dependencies
