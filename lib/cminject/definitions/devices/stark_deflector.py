cad8fb2c7632c6e1ae2f89a6f2b5a401ebf6c0c1
Yahya Saleh <yahya.saleh@desy.de>
2020-01-24 16:07:43 +0100

    A device class for stark deflector

---
+++ lib/cminject/definitions/devices/stark_deflector.py
@@ -0,0 +1,18 @@
+import numpy as np
+from typing import Tuple
+from cminject.definitions.base import Device
+from cminject.definitions.fields.stark_field import B_StarkField
+from cminject.definitions.boundaries import StarkBoundary
+from cminject.definitions.particles import Molecule
+
+class StarkDeflector(Device):
+
+    def __init__(self, filename:str, z_minmax: Tuple[float, float], *args, **kwargs):
+
+        field = B_StarkField(filename=filename)
+        boundary = StarkBoundary(field=field, z_minmax)
+        super().__init__(fields=[field], boundary=boundary)
+        # maybe I also need to create the rods of the field because they are also boundaries
+    def calculate_acceleration(self, particle: Molecule, time: float):
+
+        return self.fields[0].calculate_acceleration(particle, time)
