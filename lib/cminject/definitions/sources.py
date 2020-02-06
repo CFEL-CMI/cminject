3b85b50f9c4bc775dd41c7dff71e8c7d33b6a82d
Yahya Saleh <yahya.saleh@desy.de>
2020-02-06 15:37:28 +0100

    First draft of a source with a Boltzmann assigner.

--- lib/cminject/definitions/sources.py
+++ lib/cminject/definitions/sources.py
@@ -20,9 +20,13 @@ from typing import List, Type, Dict, Union
 import numpy as np
 from cminject.definitions.base import Source
 from cminject.definitions.particles import SphericalParticle
+from cmiinject.definitions.particles import Molecule
+import h5py as hp
 
 Distribution = Union[Dict, float]  # An appropriate type for variable distribution descriptions
+# I think The class VariableDistributionSource should be edited to accept other kind of particles.
 
+#ParticleType = Union[SphericalParticle, Molecule]
 
 class VariableDistributionSource(Source):
     """
@@ -133,6 +137,118 @@ class VariableDistributionSource(Source):
             particles.append(inst)
         return particles
 
+class MolDistributionSource(VariableDistributionSource):
+
+    def __init__(self,  number_of_particles: int,
+                position: List[Distribution],
+                velocity: List[Distribution],
+                radius: Distribution,
+                rho: float = 0,
+                seed=None,
+                randomly_rotate_around_z: bool = False,
+                subclass: subclass: Type[Molecule] = Molecule,
+                temperature: float,
+                jmax : int,
+                mass: float,
+                energy_filename: str,
+                **subclass_kwargs)):
+
+        super().__init__(number_of_particles, position, velocity, radius, rho, seed, randomly_rotate_around_z, subclass, **subclass_kwargs)
+
+    def BoltzmannAssigner(self, paths, ZeroFieldEnergies):
+        """
+        I still need to consider:
+        1- The case in which the zero energy is 0
+        2- nuclear statistics
+        3- Add a maximum zero-field energy to consider
+        4- Add tolerance
+        Note: I should check later in what units are energies stored.
+        """
+        boltz = 1.38065053e-23
+        Populations = []
+        sum = 0
+        for path, energy in zip(paths, ZeroFieldEnergies):
+            pop = math.exp(-e_0/(tempreture*boltz))
+            Populations.append(pop)
+            sum = sum+pop
+            probs.extend([pop])
+        const = 1/sum
+        probs = np.array(probs)*const
+        ParticlesAssigned = np.ceil(probs*self.number_of_particles)
+
+        return ParticlesAssigned
+
+    def PathFinder(self):
+        energy_filename = self.subclass.energy_filename
+        paths = []
+        ZeroFieldEnergies = []
+        qstates = [] # to store the quantum state of each file we want
+        def pather(name, node):
+            """
+            The script finds the paths to all energies corresponding to j<=jmax. This function works together with hp.visititems()
+            It also stores the quantum number of each state.
+            Input:
+            name: name of all paths. Usually outputed by hp.visititems
+            Output:
+            paths: a list of strings with all paths to the desired energies
+            """
+            jmax = self.jmax
+            if 'energ' in name:
+                slash_indices = []
+                dash_indices = []
+                for i, c in enumerate(name):
+                    if c == '/':
+                        slash_indices.append(i)
+                    elif c == '_':
+                        dash_indices.append(i)
+
+                J = int(name[1:indices[0]])
+
+                if J<=jmax:
+                    paths.append(energy_filename)
+                    Ka = int(name[dash_indices[1]+1, slash_indices[1]])
+                    Kc = int(name[dash_indices[2]+1, slash_indices[2]])
+                    M = int(name[dash_indices[3]+1, slash_indices[3]])
+                    Isomer = int(name[dash_indices[4]+1, slash_indices[4]])
+                    d = [J, Ka, Kc, M, Isomer]
+                    qstates.append(d)
+        with hp.File(energy_filename, 'r') as gl:
+            gl.visititems(pather)
+            # storing the free-field energy of each state.
+            for path in paths:
+                # energies in cmi_stark are not stored as numpy arrays. Should edit this later.
+                energy = gl.get(path)
+                energy = np.asarray(energy)
+                energy_0 = energy[0] # We're assuming the first point corresponds to a zero field.
+                ZeroFieldEnergies.append(energy_0)
+        return paths, ZeroFieldEnergies, np.array(qstates)
+
+    def generate_particles(self, start_time: float = 0.0):
+        position = np.array([self._generate(pdist) for pdist in self.position]).transpose()
+        if self.randomly_rotate_around_z:
+            position = self._rotate_around_z(position)
+        velocity = np.array([self._generate(vdist) for vdist in self.velocity]).transpose()
+        particles = []
+        # Get the paths, free-field energies and quantum states of quantum states that have j < jmax
+        paths, ZeroFieldEnergies, qstates = self.PathFinder()
+        # Assign weights to each quantum state
+        ParticlesAssigned = self.BoltzmannAssigner(paths, ZeroFieldEnergies)
+        m_qstates = np.repeat(qstates, ParticlesAssigned, axis = 0)
+        for i, q_state in zip(range(self.number_of_particles), m_qstates):
+            # I need to construct the q_n several times depending on ParticlesAssigned
+            d = {'J': q_state[0], 'Ka': q_state[1], 'Kc': q_state[2], 'M': q_state[3], 'Isomer':q_state[4]}
+
+            inst = self.subclass(
+                identifier=i,
+                position=np.concatenate([position[i], velocity[i]]),
+                start_time=start_time,
+                mass=self.mass,
+                q_n=d,
+                e_filename = energy_filename
+                **self.subclass_kwargs
+            )
+            particles.append(inst)
+        return particles
 
 ### Local Variables:
 ### fill-column: 100
