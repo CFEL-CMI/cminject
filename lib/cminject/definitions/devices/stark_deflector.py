8c17b8f0fc088144acc2149ff103a08b0a5e9cfd
Yahya Saleh <yahya.saleh@desy.de>
2020-02-07 15:26:43 +0100

    Updates the stark device based on the new update of the boundary.

--- lib/cminject/definitions/devices/stark_deflector.py
+++ lib/cminject/definitions/devices/stark_deflector.py
@@ -7,10 +7,10 @@ from cminject.definitions.particles import Molecule
 
 class StarkDeflector(Device):
 
-    def __init__(self, filename:str, z_minmax: Tuple[float, float], *args, **kwargs):
+    def __init__(self, filename:str, z_minmax: Tuple[float, float], field_limit: float, *args, **kwargs):
 
         field = B_StarkField(filename=filename)
-        boundary = StarkBoundary(field=field, z_minmax)
+        boundary = StarkBoundary(field=field,z_minmax=z_minmax, field_limit=field_limit)
         super().__init__(fields=[field], boundary=boundary)
         # maybe I also need to create the rods of the field because they are also boundaries
     def calculate_acceleration(self, particle: Molecule, time: float):
