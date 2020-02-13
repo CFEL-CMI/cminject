bc10ee7cb69b56bf8cae8b732f31dc8b484e7a41
Yahya Saleh <yahya.saleh@desy.de>
2020-02-13 15:45:58 +0100

    update

--- lib/cminject/definitions/devices/Skimmer.py
+++ lib/cminject/definitions/devices/Skimmer.py
@@ -6,9 +6,9 @@ class Skimmer(Device):
 
     def __init__(self, radius: float, position: np.array, *args, **kwargs):
 
-        boundary = Ring(field=field,z_minmax=z_minmax, field_limit=field_limit)
-        super().__init__(fields=[field], boundary=boundary)
+        boundary = Ring(radius=radius,position=position)
+        super().__init__(fields=[None], boundary=boundary)
         # maybe I also need to create the rods of the field because they are also boundaries
     def calculate_acceleration(self, particle: Molecule, time: float):
 
-        return self.fields[0].calculate_acceleration(particle, time)
+        return 0
