b89c5e7ebdc79994211eff9ddef2c24918e4433c
Simon Welker <simon.welker@cfel.de>
2020-07-17 16:44:37 +0200

    Export the basic setups directly from cminject.setups

--- lib/cminject/setups/__init__.py
+++ lib/cminject/setups/__init__.py
@@ -15,6 +15,11 @@
 # You should have received a copy of the GNU General Public License along with this program. If not, see
 # <http://www.gnu.org/licenses/>.
 
+__all__ = ['OneFlowFieldSetup', 'DesyatnikovPhotophoresisSetup']
+
+from .one_flow_field import OneFlowFieldSetup
+from .desyatnikov_photophoresis import DesyatnikovPhotophoresisSetup
+
 ### Local Variables:
 ### fill-column: 100
 ### truncate-lines: t
