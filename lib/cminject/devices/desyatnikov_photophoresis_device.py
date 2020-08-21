048533e36bc818467094c614b1795902e34c9924
Simon Welker <simon.welker@cfel.de>
2020-08-21 11:47:41 +0200

    Shorten photophoresis setup and add air drag to the device by default

--- lib/cminject/devices/desyatnikov_photophoresis_device.py
+++ lib/cminject/devices/desyatnikov_photophoresis_device.py
@@ -19,7 +19,9 @@ from typing import Tuple, Union
 
 from cminject.base import Device
 from cminject.boundaries.simple import CuboidBoundary
+from cminject.calc.fluid_flow import a_stokes
 from cminject.fields.fluid_flow import StokesDragForceField
+from cminject.fields.function_based import FunctionField
 from cminject.fields.photophoresis import DesyatnikovPhotophoreticLaserField
 
 
@@ -28,11 +30,14 @@ class DesyatnikovPhotophoresisDevice(Device):
     A Device for simulating photophoretic effects with the model described in: A. Desyatnikov, V. Shvedov, A. Rode,
     W. Krolikowski, and Y. Kivshar, "Photophoretic manipulation of absorbing aerosol particles with vortex beams: theory
     versus experiment," Opt. Express  17, 8201-8211 (2009).
+
+    Adds a drag field for a static fluid (v=0 everywhere) with the given gas viscosity and no slip correction,
+    unless ``static_drag=False`` is passed.
     """
-    def __init__(self, r_boundary: Tuple[float, float], z_boundary: Tuple[float, float],
-                 gas_temperature: float, gas_viscosity: float, gas_thermal_conductivity: float,
-                 gas_density: Union[float, Tuple[float, StokesDragForceField]], gas_mass: float,
-                 beam_power: float, beam_waist_radius: float,  z_position: float = 0.0):
+    def __init__(self, r_boundary: Tuple[float, float], z_boundary: Tuple[float, float], gas_temperature: float,
+                 gas_viscosity: float, gas_thermal_conductivity: float, gas_mass: float,
+                 gas_density: Union[float, Tuple[float, StokesDragForceField]],
+                 beam_power: float, beam_waist_radius: float, z_position: float = 0.0, static_drag: bool = True):
         """
         The constructor for DesyatnikovPhotophoresisDevice.
 
@@ -41,20 +46,23 @@ class DesyatnikovPhotophoresisDevice(Device):
         :param gas_temperature: The temperature of the gas the laser is positioned in [K]
         :param gas_viscosity: The viscosity of the gas the laser is positioned in [Pa*s]
         :param gas_thermal_conductivity: The thermal conductivity of the gas the laser is positioned in [W/(m*K)]
+        :param gas_mass: The mass of a single gas molecule [kg]
         :param gas_density: The density of the gas the laser is positioned in, either as a fixed value or a tuple of a
             default value (a float) and a flow field to base the density on an interpolated pressure.
-        :param gas_mass: The mass of a single gas molecule [kg]
         :param beam_power: The total power of the laser [W]
         :param beam_waist_radius: The radius of the laser beam waist [m]
         :param z_position: The Z position of the laser beam waist (0.0 by default)
+        :param static_drag: True if a static (v=0) Stokesian fluid field should be added to model drag resistance,
+            False if it should not. True by default.
         """
         pp_field = DesyatnikovPhotophoreticLaserField(
             gas_viscosity=gas_viscosity, gas_temperature=gas_temperature,
             gas_thermal_conductivity=gas_thermal_conductivity, gas_density=gas_density, gas_mass=gas_mass,
             beam_power=beam_power, beam_waist_radius=beam_waist_radius, z_position=z_position
         )
+        air_drag_field = FunctionField(lambda p, t: a_stokes(-p.velocity, gas_viscosity, p.radius, p.mass, 1.0))
         boundary: CuboidBoundary = CuboidBoundary(intervals=[r_boundary, z_boundary])
-        super().__init__(fields=[pp_field], boundary=boundary)
+        super().__init__(fields=[pp_field, air_drag_field], boundary=boundary)
 
 ### Local Variables:
 ### fill-column: 100
--- lib/cminject/setups/desyatnikov_photophoresis.py
+++ lib/cminject/setups/desyatnikov_photophoresis.py
@@ -23,26 +23,17 @@ versus experiment," Opt. Express  17, 8201-8211 (2009).
 
 import argparse
 
-from cminject.base import Particle, Action, Setup
+import numpy as np
+
+from cminject.base import Setup, Device
+from cminject.boundaries.simple import InfiniteBoundary
 from cminject.detectors.simple_z import SimpleZDetector
 from cminject.devices.desyatnikov_photophoresis_device import DesyatnikovPhotophoresisDevice
+from cminject.experiment import Experiment
+from cminject.fields.function_based import FunctionField
 from cminject.particles.spherical import ThermallyConductiveSphericalParticle
 from cminject.sources.variable_distributions import VariableDistributionSource
-from cminject.experiment import Experiment
 from cminject.utils.args import dist_description, SetupArgumentParser
-
-M_GAS_AIR = 5.6e-26
-
-
-class MarkAsLostWhenVZIsPositive(Action):
-    """
-    Marks particles as lost when their z coordinate and v_z are both positive.
-    Since the beam only drives particles away from the source position in this area, we can consider the particles lost.
-    """
-    def __call__(self, particle: Particle, time: float) -> bool:
-        if particle.position[-1] > 0.0 and particle.velocity[-1] > 0.0:
-            particle.lost = True
-        return False
 
 
 class DesyatnikovPhotophoresisSetup(Setup):
@@ -53,84 +44,71 @@ class DesyatnikovPhotophoresisSetup(Setup):
     """
     @staticmethod
     def construct_experiment(main_args: argparse.Namespace, args: argparse.Namespace) -> Experiment:
-        experiment = Experiment(
-            time_interval=main_args.time_interval, time_step=main_args.time_step,
-            random_seed=main_args.seed, number_of_dimensions=2
-        )
-
+        experiment = Experiment(time_interval=main_args.time_interval, time_step=main_args.time_step,
+                                random_seed=main_args.seed, number_of_dimensions=2)
         experiment.add_source(VariableDistributionSource(
             main_args.nof_particles, position=args.position, velocity=args.velocity, radius=args.radius,
             density=args.rho, subclass=ThermallyConductiveSphericalParticle,
-            subclass_kwargs={
-                'specific_heat': 0.0, 'temperature': 293.15, 'thermal_conductivity': args.thermal_conductivity
-            }
+            subclass_kwargs={'specific_heat': 0.0, 'temperature': 293.15,
+                             'thermal_conductivity': args.gas_thermal_conductivity}
         ))
-
         experiment.add_device(DesyatnikovPhotophoresisDevice(
             r_boundary=args.boundary[:2], z_boundary=args.boundary[2:],
-            gas_temperature=args.gas_temperature, gas_viscosity=args.gas_viscosity,
-            gas_thermal_conductivity=args.gas_thermal_conductivity, gas_density=args.gas_density, gas_mass=M_GAS_AIR,
-            beam_power=args.beam_power, beam_waist_radius=args.beam_waist_radius,
+            gas_temperature=args.gas_temperature, gas_viscosity=args.gas_viscosity, gas_mass=args.gas_mass,
+            gas_thermal_conductivity=args.gas_thermal_conductivity, gas_density=args.gas_density,
+            beam_power=args.beam_power, beam_waist_radius=args.beam_waist_radius
         ))
-
+        if args.gravity:
+            experiment.add_device(
+                Device(boundary=InfiniteBoundary(), fields=[FunctionField(lambda p, t: np.array([0, -9.81]))])
+            )
         for i, pos in enumerate(args.detectors):
             experiment.add_detector(SimpleZDetector(identifier=i, z_position=pos))
 
+        experiment.z_boundary = tuple(args.boundary[2:])
         return experiment
 
     @staticmethod
     def get_parser() -> SetupArgumentParser:
         parser = SetupArgumentParser()
-        parser.add_argument('-d', '--detectors', help='The Z positions of the detectors', nargs='+',
-                            type=float, required=True)
-        parser.add_argument('-p', '--position',
-                            help='Distribution description for the position.',
-                            nargs='*', type=dist_description)
-        parser.add_argument('-v', '--velocity',
-                            help='Distribution description for the velocity.',
-                            nargs='*', type=dist_description)
-        parser.add_argument('-r', '--radius', help='Distribution description for the radius.', type=dist_description)
-        parser.add_argument('-rho', '--density', help='Density of the particles.', type=float)
-        parser.add_argument('-mu', '--thermal-conductivity', help='Thermal conductivity of the particles.', type=float)
+        add_arg = lambda *args, **kwargs: parser.add_argument(*args, **kwargs)  # Shorthand functions for readability
+        add_float_arg = lambda *args, **kwargs: add_arg(*args, **kwargs, type=float)
+        add_dist_arg = lambda *args, **kwargs: add_arg(*args, **kwargs, type=dist_description)
 
-        parser.add_argument('-bp', '--beam-power', help='Power of the laser beam in watts', type=float)
-        parser.add_argument('-br', '--beam-waist-radius', help='The waist radius of the LG01 beam (w_0)', type=float)
+        add_float_arg('-d', '--detectors', help='The Z positions of the detectors', nargs='+', required=True)
+        add_dist_arg('-p', '--position', help='Distribution description for the position.', nargs='*')
+        add_dist_arg('-v', '--velocity', help='Distribution description for the velocity.', nargs='*')
+        add_dist_arg('-r', '--radius', help='Distribution description for the radius.')
+        add_float_arg('-rho', '--density', help='Density of the particles.')
+        add_float_arg('-mu', '--thermal-conductivity', help='Thermal conductivity of the particles.')
 
-        parser.add_argument('-gt', '--gas-temperature', help='The temperature of the surrounding gas [K].', type=float)
-        parser.add_argument('-gv', '--gas-viscosity', help='The dynamic viscosity of the surrounding gas [Pa*s].',
-                            type=float)
-        parser.add_argument('-gmu', '--gas-thermal-conductivity',
-                            help='The thermal conductivity of the surrounding gas [W m^-1 K^-1].', type=float)
-        parser.add_argument('-gd', '--gas-density', help='The density of the surrounding gas  [kg m^-3].', type=float)
+        add_float_arg('-bp', '--beam-power', help='Power of the laser beam in watts')
+        add_float_arg('-br', '--beam-waist-radius', help='The waist radius of the LG01 beam (w_0)')
 
-        parser.add_argument('-b', '--boundary', help='Boundary (rmin, rmax, zmin, zmax) of the experiment.',
-                            type=float, nargs=4)
+        add_float_arg('-gt', '--gas-temperature', help='The temperature of the surrounding gas [K].')
+        add_float_arg('-gv', '--gas-viscosity', help='The dynamic viscosity of the surrounding gas [Pa*s].')
+        add_float_arg('-gmu', '--gas-thermal-conductivity',
+                      help='The thermal conductivity of the surrounding gas [W m^-1 K^-1].')
+        add_float_arg('-gd', '--gas-density', help='The density of the surrounding gas  [kg m^-3].')
+
+        add_arg('-G', '--gravity', help='Add a constant Z acceleration of -9.81 m/s^2.', action='store_true')
+        add_float_arg('-b', '--boundary', help='Boundary (rmin, rmax, zmin, zmax) of the experiment.', nargs=4)
 
         parser.set_defaults(
             # Defaults given to me by Andrei Rode
-            position=[{'kind': 'radial_gaussian', 'mu': 0.0, 'sigma': 20e-6},
-                      0.9e-3],
-            velocity=[{'kind': 'gaussian', 'mu': 0.0, 'sigma': 0.01},
-                      {'kind': 'gaussian', 'mu': -10.0, 'sigma': 0.01}],
+            position=[{'kind': 'radial_gaussian', 'mu': 0.0, 'sigma': 20e-6}, 0.9e-3],
+            velocity=[{'kind': 'gaussian', 'mu': 0.0, 'sigma': 0.01}, {'kind': 'gaussian', 'mu': -10.0, 'sigma': 0.01}],
             radius={'kind': 'gaussian', 'mu': 2e-6, 'sigma': 10e-9},
-
-            # Add a uniform flow field
-            uniform_flow_velocity=None,
-
             # Assume polystyrene spheres
             rho=1050.0,  # [kg/(m^3)]
             thermal_conductivity=0.030,  # [W/(m*K)]
-
-            # Assume air at 293.15K, all values taken from https://www.engineeringtoolbox.com/
-            gas_temperature=293.15,  # [K]
-            gas_viscosity=1.82e-5,  # [Pa*s]
-            gas_thermal_conductivity=25.87e-3,  # [W/(m*K)]
-            gas_density=1.204,  # [kg/(m^3)]
-
+            # Assume air at 293.15K, all values taken from https://www.engineeringtoolbox.com/. All values given are in
+            # SI base units, e.g. the gas density is in [kg m^-3], the thermal conductivity in [W m^-1 K^-1)]
+            gas_temperature=293.15, gas_viscosity=1.82e-5, gas_thermal_conductivity=25.87e-3, gas_density=1.204,
+            gas_mass=4.81e-26,  # for an 'average' air particle (molar mass divided by Avogadro's constant)
             beam_power=1.0,  # [W]
             beam_waist_radius=4.0e-6,  # [m]
-
-            boundary=[-1e-4, 1e-4, -1e-3, 1e-3],  # [m]
+            boundary=[-1e-4, 1e-4, -1e-3, 1e-3]  # [m]
         )
         return parser
 
