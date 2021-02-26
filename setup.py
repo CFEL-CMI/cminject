80ebe29718d1dc8521018cb48789523061aa5588
Simon Welker <6welker@informatik.uni-hamburg.de>
2021-02-26 14:12:12 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from Cython.Build import cythonize
 import numpy
 
 name = 'cminject'
-version = '1.0.5'
+version = '1.0.6'
 release = version
 author = 'Simon Welker, Muhamed Amin, and the CFEL Controlled Molecule Imaging group'
 copyright = f'2021, {author}'
