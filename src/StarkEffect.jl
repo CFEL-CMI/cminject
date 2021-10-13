d88a0fd86b1f5de5bd272966169c265564169fa1
Timo Borner <timo.borner@cfel.de>
2021-10-13 16:15:09 +0200

    Use SplitApplyCombine to transpose jugged vector

--- src/StarkEffect.jl
+++ src/StarkEffect.jl
@@ -1,5 +1,6 @@
 using Parameters
 using LinearAlgebra
+using SplitApplyCombine
 
 @with_kw struct StarkCurve{T}
     ΔE::T
@@ -9,18 +10,14 @@ end
 
 # For linear top (recognizable by the quantum numbers)
 # with completed shells (hence integer quantum numbers)
+# TODO: Validate results
 function calculateStarkCurves(ΔE::T, E_min::T, E_max::T,
         # TODO: It might be cleaner to just pass the particle directly
         J_min::Int, J_max::Int, M::Int, B::T, D::T, μ::T)::Vector{StarkCurve{T}} where T
     fieldJEnergy = map(E -> calculateEnergies(J_min, J_max, M, E, B, D, μ), E_min:ΔE:E_max)
     # We now have field -> J -> energy, but want J -> field -> energy
-    jFieldEnergy = transposeJugged(fieldJEnergy)
+    jFieldEnergy = invert(fieldJEnergy)
     map(energies -> StarkCurve(ΔE=ΔE, E_min=E_min, energies=energies), jFieldEnergy)
-end
-
-function transposeJugged(m::Vector{Vector{T}}) where T
-    matrixM=reshape(collect(Iterators.flatten(m)), (size(m[1],1),size(m,1)))
-    map(i->matrixM[i,:], 1:size(matrixM,1))
 end
 
 function calculateEnergies(J_min::Int, J_max::Int, M::Int, E::T,
@@ -36,14 +33,7 @@ end
 # D is the centrifugal distortion constant
 function getHamiltonian(J_min::Int, J_max::Int, M::Int, E::T,
         B::T, D::T, μ::T)::Matrix{T} where T
-    # TODO
     fieldFree = map(J -> B * J*(J+1) - D * (J*(J+1))^2, J_min:J_max)
     offDiagonal = map(J -> -μ * E * √((J+1)^2-M^2) / √((2*J+1) * (2*J+3)), J_min:J_max-1)
     Tridiagonal(offDiagonal, fieldFree, offDiagonal)
 end
-
-
-# B -> rotcon   -> 5000e5   -> 3.3e-25
-# D -> quartic  -> 50       -> 3.3e-32
-# μ -> dipole   -> 5        -> 1.7e-29
-# E -> dcfield  -> 100      -> 1e-2
