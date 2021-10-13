257cf1cde62ab4d7bf8cab04d6667a31c7a8e193
Timo Borner <timo.borner@cfel.de>
2021-10-13 16:03:25 +0200

    First stark curves

--- src/StarkEffect.jl
+++ src/StarkEffect.jl
@@ -9,13 +9,41 @@ end
 
 # For linear top (recognizable by the quantum numbers)
 # with completed shells (hence integer quantum numbers)
-function calculateStarkCurve(ΔE::T, E_min::T, E_max::T,
-        J_min::Int, J_max::Int, M::Int)::Vector{StarkCurve{T}} where T
-    # TODO
-    [StarkCurve(ΔE=ΔE, E_min=E_min, energies=[1.0,2.0,3.0])]
+function calculateStarkCurves(ΔE::T, E_min::T, E_max::T,
+        # TODO: It might be cleaner to just pass the particle directly
+        J_min::Int, J_max::Int, M::Int, B::T, D::T, μ::T)::Vector{StarkCurve{T}} where T
+    fieldJEnergy = map(E -> calculateEnergies(J_min, J_max, M, E, B, D, μ), E_min:ΔE:E_max)
+    # We now have field -> J -> energy, but want J -> field -> energy
+    jFieldEnergy = transposeJugged(fieldJEnergy)
+    map(energies -> StarkCurve(ΔE=ΔE, E_min=E_min, energies=energies), jFieldEnergy)
 end
 
-function getHamiltonian(J_min::Int, J_max::Int, M::Int, E::T)::Matrix{T} where T
-    # TODO
-    Tridiagonal(collect(J_min+1:J_max), collect(J_min:J_max), collect(J_min:J_max-1))
+function transposeJugged(m::Vector{Vector{T}}) where T
+    matrixM=reshape(collect(Iterators.flatten(m)), (size(m[1],1),size(m,1)))
+    map(i->matrixM[i,:], 1:size(matrixM,1))
 end
+
+function calculateEnergies(J_min::Int, J_max::Int, M::Int, E::T,
+        B::T, D::T, μ::T)::Vector{T} where T
+    hamiltonian = getHamiltonian(J_min, J_max, M, E, B, D, μ)
+    sort(eigvals(hamiltonian))
+end
+
+# J_min and J_max denote the range of the J quantum number,
+# M is the specific value of the M quantum number,
+# E is the field strength,
+# B is a rotational constant and
+# D is the centrifugal distortion constant
+function getHamiltonian(J_min::Int, J_max::Int, M::Int, E::T,
+        B::T, D::T, μ::T)::Matrix{T} where T
+    # TODO
+    fieldFree = map(J -> B * J*(J+1) - D * (J*(J+1))^2, J_min:J_max)
+    offDiagonal = map(J -> -μ * E * √((J+1)^2-M^2) / √((2*J+1) * (2*J+3)), J_min:J_max-1)
+    Tridiagonal(offDiagonal, fieldFree, offDiagonal)
+end
+
+
+# B -> rotcon   -> 5000e5   -> 3.3e-25
+# D -> quartic  -> 50       -> 3.3e-32
+# μ -> dipole   -> 5        -> 1.7e-29
+# E -> dcfield  -> 100      -> 1e-2
