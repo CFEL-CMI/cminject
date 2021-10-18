4b837887e23b41a762c248f188818b8ab016820e
MetaColon <timo@teemze.de>
2021-10-18 15:06:00 +0200

    More documentation

--- src/StarkEffect.jl
+++ src/StarkEffect.jl
@@ -1,7 +1,17 @@
+# This whole file is heavily inspired by the CMIstark project, see https://doi.org/10.1016/j.cpc.2013.09.001
 using Parameters
 using LinearAlgebra
 using SplitApplyCombine
 
+"""
+    StarkCurve
+
+Represents a Stark curve, that is the energies over a certain range of field strengths.
+
+This is sampled (not continuous) with a step size of `ΔE`. The start is `E_min`.
+
+The field strengths are given in Volts/Meter [V/m] and the energies in Joule [J].
+"""
 @with_kw struct StarkCurve{T<:Real}
     ΔE::T
     E_min::T
@@ -17,15 +27,18 @@ This is done for multiple `J` quantum numbers at once (from `J_min` to `J_max`),
 Note that while usually the rotational constants are given in Hertz, here Joules are used directly.
 
 # Arguments
-- `ΔE`: The field strength step size
-- `E_min`: The starting field strength
-- `E_max`: The ending field strength
+- `ΔE`: The field strength step size. Unit: Volts/Meter [V/m]
+- `E_min`: The starting field strength. Unit: Volts/Meter [V/m]
+- `E_max`: The ending field strength. Unit: Volts/Meter [V/m]
 - `J_min::I`: The minimum J quantum number
 - `J_max::I`: The maximum quantum number
 - `M::I`: The M quantum number
 - `B`: The second rotational constant, B=h/(8π^2⋅I_b). Unit: Joule [J]
 - `D`: The centrifugal distortion constant. Unit: Joule [J]
 - `μ`: The dipole moment along its symmetry axis. Unit: Coulomb⋅Meter [Cm]
+
+# Returns
+Returns a vector of the calculated Stark curves
 """
 function calculateStarkCurves(ΔE, E_min, E_max,
         # TODO: It might be cleaner to just pass the particle directly
@@ -49,10 +62,13 @@ Note that while usually the rotational constants are given in Hertz, here Joules
 - `J_min::I`: The minimum J quantum number
 - `J_max::I`: The maximum quantum number
 - `M::I`: The M quantum number
-- `E`: The field strength
+- `E`: The field strength. Unit: Volts/Meter [V/m]
 - `B`: The second rotational constant, B=h/(8π^2⋅I_b). Unit: Joule [J]
 - `D`: The centrifugal distortion constant. Unit: Joule [J]
 - `μ`: The dipole moment along its symmetry axis. Unit: Coulomb⋅Meter [Cm]
+
+# Returns
+Returns a vector of the calculated energies. Unit: Joule [J]
 """
 function calculateEnergies(J_min::I, J_max::I, M::I, E, B, D, μ) where I <: Integer
     hamiltonian = getHamiltonian(J_min, J_max, M, E, B, D, μ)
@@ -62,19 +78,22 @@ end
 """
     getHamiltonian(J_min, J_max, M, E, B, D, μ)
 
-Calculates the hamiltonian for a lienar top molecule with completed shells.
+Calculates the Hamiltonian for a lienar top molecule with completed shells.
 This is done for the given range of `J` quantum numbers, the `M` quantum number and the field strength.
-Note that this is only one block of the whole block-diagonalized hamiltonian consisting of all the `M` quantum numbers.
+Note that this is only one block of the whole block-diagonalized Hamiltonian consisting of all the `M` quantum numbers.
 Note that while usually the rotational constants are given in Hertz, here Joules are used directly.
 
 # Arguments
 - `J_min::I`: The minimum J quantum number
 - `J_max::I`: The maximum quantum number
 - `M::I`: The M quantum number
-- `E`: The field strength
+- `E`: The field strength. Unit: Volts/Meter [V/m]
 - `B`: The second rotational constant, B=h/(8π^2⋅I_b). Unit: Joule [J]
 - `D`: The centrifugal distortion constant. Unit: Joule [J]
 - `μ`: The dipole moment along its symmetry axis. Unit: Coulomb⋅Meter [Cm]
+
+# Returns
+Returns an AbstractMatrix (specifically a symmetrical tridiagonal matrix) representing the Hamiltonian.
 """
 function getHamiltonian(J_min::I, J_max::I, M::I, E, B, D, μ) where I <: Integer
     fieldFree = [B * J*(J+1) - D * (J*(J+1))^2 for J ∈ J_min:J_max]
