874948e3fa7c61d68c53907f26fb546cdbf56d7d
MetaColon <timo@teemze.de>
2021-10-18 14:24:18 +0200

    Documentation

--- src/StarkEffect.jl
+++ src/StarkEffect.jl
@@ -9,8 +9,22 @@ using SplitApplyCombine
     energies::Vector{T}
 end
 
-# For linear top (recognizable by the quantum numbers)
-# with completed shells (hence integer quantum numbers)
+"""
+    calculateStarkCurves(ΔE, E_min, E_max, J_min, J_max, M, B, D, μ)
+
+Calculates the Stark curves for a linear top molecule with completed shells.
+This is done for multiple J quantum numbers at once (from J_min to J_max), but only for one M quantum number.
+# Arguments
+- `ΔE`: The field strength step size
+- `E_min`: The starting field strength
+- `E_max`: The ending field strength
+- `J_min::I`: The minimum J quantum number
+- `J_max::I`: The maximum quantum number
+- `M::I`: The M quantum number
+- `B`: The second rotational constant, B=h/(8π^2⋅I_b)
+- `D`: The centrifugal distortion constant
+- `μ`: The dipole moment along its symmetry axis
+"""
 function calculateStarkCurves(ΔE, E_min, E_max,
         # TODO: It might be cleaner to just pass the particle directly
         J_min::I, J_max::I, M::I, B, D, μ) where I <: Integer
@@ -20,16 +34,40 @@ function calculateStarkCurves(ΔE, E_min, E_max,
     StarkCurve.(float(ΔE), float(E_min), jFieldEnergy)
 end
 
+"""
+    calculateEnergies(J_min, , J_max, M, E, B, D, μ)
+
+Calculates the energies for the given J quantum number range, M quantum number and field strength.
+The result is ordered by the J quantum number in ascending order (which is also the ascending order
+of the energies themselves).
+# Arguments
+- `J_min::I`: The minimum J quantum number
+- `J_max::I`: The maximum quantum number
+- `M::I`: The M quantum number
+- `E`: The field strength
+- `B`: The second rotational constant, B=h/(8π^2⋅I_b)
+- `D`: The centrifugal distortion constant
+- `μ`: The dipole moment along its symmetry axis
+"""
 function calculateEnergies(J_min::I, J_max::I, M::I, E, B, D, μ) where I <: Integer
     hamiltonian = getHamiltonian(J_min, J_max, M, E, B, D, μ)
     sort(eigvals(hamiltonian))
 end
 
-# J_min and J_max denote the range of the J quantum number,
-# M is the specific value of the M quantum number,
-# E is the field strength,
-# B is a rotational constant and
-# D is the centrifugal distortion constant
+"""
+    getHamiltonian(J_min, J_max, M, E, B, D, μ)
+
+Calculates the hamiltonian for the given range of J quantum numbers, the M quantum number and the field strength.
+Note that this is only one block of the whole block-diagonalized hamiltonian consisting of all the M quantum numbers.
+# Arguments
+- `J_min::I`: The minimum J quantum number
+- `J_max::I`: The maximum quantum number
+- `M::I`: The M quantum number
+- `E`: The field strength
+- `B`: The second rotational constant, B=h/(8π^2⋅I_b)
+- `D`: The centrifugal distortion constant
+- `μ`: The dipole moment along its symmetry axis
+"""
 function getHamiltonian(J_min::I, J_max::I, M::I, E, B, D, μ) where I <: Integer
     fieldFree = [B * J*(J+1) - D * (J*(J+1))^2 for J ∈ J_min:J_max]
     offDiagonal = [-μ * E * √((J+1)^2-M^2) / √((2*J+1) * (2*J+3)) for J ∈ J_min:J_max-1]
