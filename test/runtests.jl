db733674cd3b0c98f69c38ad51b5bc3b6b58b764
MetaColon <timo@teemze.de>
2021-10-19 10:07:46 +0200

    Check for hermitian Hamiltonian & include file

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using CMInject
+include("../src/StarkEffect.jl")
 using Test
 
 @testset "StarkEffect" begin
@@ -7,4 +8,5 @@ using Test
     @test isa(calculateEnergies(1, 5, 1, 1, 1, 1, 1), Vector)
     @test isa(calculateStarkCurves(1, 1, 10, 1, 5, 1, 1, 1, 1), Vector)
     @test all(curve -> isa(curve, StarkCurve), calculateStarkCurves(1, 1, 10, 1, 5, 1, 1, 1, 1))
+    @test all(x -> abs(x) < 0.000001, getHamiltonian(1, 5, 1, 1, 1, 1, 1)' - getHamiltonian(1, 5, 1, 1, 1, 1, 1))
 end
