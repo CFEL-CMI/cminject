149591c2d32c85c13cb32aaf84af4f1af67e741a
Simon Welker <6welker@informatik.uni-hamburg.de>
2021-11-05 09:45:14 +0100

    Use 2 threads for creating the sysimage (so that multithreading code is employed)

--- build/create_sysimage.sh
+++ build/create_sysimage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 tmpout="tmpout.h5"
 
-julia --project=@. --startup-file=no --trace-compile=precompile_statements.jl ../run_cminject.jl \
+julia -t 2 --project=@. --startup-file=no --trace-compile=precompile_statements.jl ../run_cminject.jl \
     -f ../data/nitrogen_1.8mbar_extended_nan.h5 -n 100\
     --x G[0,1e-3] --z -0.1285 --vx G[0,0.1] --vz 10 --r 13.5e-9\
     --rho 19320 -d -0.001 0.0 0.001\
