fedb6faae427641e9c668f1a4ec6645bf66a422a
MetaColon <timo@teemze.de>
2021-11-16 17:22:11 +0100

    Fix test (partially)

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,11 +11,11 @@ using Test
     itpScaled = CMInject.itpscale(itp, initial[1]:stepSizes[1]:(initial[1]+(counts[1]-1)*stepSizes[1]), initial[2]:stepSizes[2]:(initial[2]+(counts[2]-1)*stepSizes[2]))
     dists = Dict(:x => CMInject.Normal(0, 1e-3), :y => CMInject.Dirac(-0.128), :vx => CMInject.Normal(0, 0.1), :vy => CMInject.Dirac(10.0), :m => CMInject.Dirac(2))
     stateDists = Dict(:J => CMInject.DiscreteUniform(2,5), :M => CMInject.DiscreteUniform(0,2))
-    source = CMInject.StarkSamplingSource{CMInject.StarkParticle2D{Float64}, Float64}(dists, stateDists, "../cmistark/OCS.molecule")
+    source = CMInject.StarkSamplingSource{CMInject.StarkParticle2D{Float64}, Float64}(dists, stateDists, "test/OCS.molecule")
 
     @test CMInject.generate(source, 1) != nothing
 
-    field = CMInject.ElectricField2d(itpScaled)
+    field = CMInject.ElectricField2D(itpScaled)
     experiment = CMInject.Experiment(;source=source, n_particles=10, fields=(field,), detectors=Tuple(CMInject.SectionDetector{Float64,:y}.(-0.003:0.001:0.003, true)), solver=CMInject.EulerHeun(), time_span=(0.0, 0.03), time_step=1e-5, ensemble_alg=CMInject.EnsembleThreads())
     simRes = CMInject.simulate(experiment)
     data = simRes[2]
