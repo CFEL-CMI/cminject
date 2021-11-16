536bbf8526dd656afcc5d853a8ab26a72361821d
MetaColon <timo@teemze.de>
2021-11-16 17:12:23 +0100

    Correctly read field

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using CMInject
 using Test
 
 @testset "Stark Simulation" begin
-    fieldLines=readlines("example_field")
+    fieldLines=readlines("test/example_field")
     initial = [parse(Float64, token) for token ∈ split(fieldLines[1], " ") if length(token) > 0]
     stepSizes = [parse(Float64, token) for token ∈ split(fieldLines[2], " ") if length(token) > 0]
     counts = [parse(Int, token) for token ∈ split(fieldLines[3], " ") if length(token) > 0]
@@ -11,6 +11,7 @@ using Test
     itpScaled = CMInject.itpscale(itp, initial[1]:stepSizes[1]:(initial[1]+(counts[1]-1)*stepSizes[1]), initial[2]:stepSizes[2]:(initial[2]+(counts[2]-1)*stepSizes[2]))
     dists = Dict(:x => CMInject.Normal(0, 1e-3), :y => CMInject.Dirac(-0.128), :vx => CMInject.Normal(0, 0.1), :vy => CMInject.Dirac(10.0), :m => CMInject.Dirac(2))
     stateDists = Dict(:J => CMInject.DiscreteUniform(2,5), :M => CMInject.DiscreteUniform(0,2))
+    # TODO: Add molecule to git
     source = CMInject.StarkSamplingSource{CMInject.StarkParticle2D{Float64}, Float64}(dists, stateDists, "../cmistark/OCS.molecule")
 
     @test CMInject.generate(source, 1) != nothing
