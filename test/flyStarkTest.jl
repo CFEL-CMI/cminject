080189f3f2f41d71dccac1788c04a4f35c881c3e
MetaColon <timo@teemze.de>
2021-12-14 17:24:15 +0100

    Preparations for Fly Stark Test

---
+++ test/flyStarkTest.jl
@@ -0,0 +1,4 @@
+@testset "Fly Stark Simulation" begin
+    # TODO: Setup from CMIfly and override `unstable_check` to hardcode devices
+    @test 1 == 1
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,40 +1,5 @@
 using CMInject
 using Test
 using Formatting
-
-@testset "Stark Simulation" begin
-    grid = [1e7 2e7; 1e7 2e7]
-    itp = CMInject.interpolate(grid, CMInject.BSpline(CMInject.Linear()))
-    ext = CMInject.extrapolate(itp, 0)
-    itpScaled = CMInject.itpscale(ext, 0:1:1, -0.5:1:0.5)
-
-    dists = Dict(:x => CMInject.Dirac(0), :y => CMInject.Dirac(0), :vx => CMInject.Dirac(1), :vy => CMInject.Dirac(0), :m => CMInject.Dirac(1e-9))
-    stateDists = Dict(:J => CMInject.DiscreteUniform(0,0), :M => CMInject.DiscreteUniform(0,0))
-    sourcePyrroleWater = CMInject.StarkSamplingSource{CMInject.StarkParticle2D{Float64}, Float64}(
-                                     dists, stateDists, "test/pyrrole-water.molecule")
-
-    @test CMInject.generate(sourcePyrroleWater, 1) != nothing
-
-    field = CMInject.ElectricField2D(itpScaled)
-    particles = 5
-    experimentPyrroleWater = CMInject.Experiment(;source=sourcePyrroleWater,
-                                            n_particles=particles,
-                                            fields=(field,),
-                                            detectors=Tuple(CMInject.SectionDetector{Float64,:y}.(
-                                                             -0.001:0.001:0.001,
-                                                             true)),
-                                            solver=CMInject.EulerHeun(),
-                                            time_span=(0.0, 1.0),
-                                            time_step=1e-1,
-                                            ensemble_alg=CMInject.EnsembleThreads())
-    simResPyrroleWater = CMInject.simulate(experimentPyrroleWater)
-    dataPyrroleWater = simResPyrroleWater[1]
-
-    @test simResPyrroleWater != nothing
-    @test dataPyrroleWater != nothing
-
-    for i in 1:particles
-        @test abs(last(dataPyrroleWater[i].u).y + 6.653e-14) < 0.01e-14
-        print(last(dataPyrroleWater[i].u).y, "\n")
-    end
-end
+include("simpleStarkTest.jl")
+include("flyStarkTest.jl")
