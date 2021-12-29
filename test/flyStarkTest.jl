35a37e3181ed0890be531b18658495cf23773105
Timo Borner <timo.borner@cfel.de>
2021-12-29 12:34:05 +0100

    Add devices & collisions

--- test/flyStarkTest.jl
+++ test/flyStarkTest.jl
@@ -7,7 +7,11 @@
     itpScaled = CMInject.itpscale(ext, 0:1:1, -0.5:1:0.5)
 
     # TODO: Normal distribution of velocities and initial positions
-    dists = Dict(:x => CMInject.Dirac(0), :y => CMInject.Dirac(0), :vx => CMInject.Dirac(1), :vy => CMInject.Dirac(0), :m => CMInject.Dirac((4*12+7*1.0078+14.003+15.995)/9.223e18)
+    dists = Dict(:x => CMInject.Dirac(0),
+                 :y => CMInject.Dirac(0),
+                 :vx => CMInject.Dirac(0),
+                 :vy => CMInject.Dirac(1),
+                 :m => CMInject.Dirac((4*12+7*1.0078+14.003+15.995)/9.223e18)
     stateDists = Dict(:J => CMInject.DiscreteUniform(0,0), :M => CMInject.DiscreteUniform(0,0))
     sourcePyrroleWater = CMInject.StarkSamplingSource{CMInject.StarkParticle2D{Float64}, Float64}(
                                      dists, stateDists, "test/pyrrole-water.molecule")
@@ -18,6 +22,34 @@
     particles = 5
     @inline function detectHits(args...)
         print("Args: ", args[2].x, ",", args[2].y, "\n")
+        x = args[2].x
+        y = args[2].y
+        # TODO: There are contradictoray information for these values
+        skimmer1Y = 0.065
+        skimmer2Y = 0.302
+        knifeY    = 0.512
+        knifeX    = 0
+        skimmer3Y = 0.537
+        skimmer1R = 0.003
+        skimmer2R = 0.0015
+        skimmer3R = 0.0015
+        # TODO: Don't ignore deflector hits
+        if (y ≥ skimmer1Y-0.01 && y ≤ skimmer1Y &&
+            abs(x) ≤ skimmer1R)
+            return true;
+        end
+        if (y ≥ skimmer2Y-0.01 && y ≤ skimmer2Y &&
+            abs(x) ≤ skimmer2R)
+            return true;
+        end
+        if (y ≥ skimmer3Y-0.01 && y ≤ skimmer3Y &&
+            abs(x) ≤ skimmer3R)
+            return true;
+        end
+        if (y ≥ knifeY-0.01 && y ≤ knifeY &&
+            x ≤ knifeX)
+            return true;
+        end
         false
     end
     experimentPyrroleWater = CMInject.Experiment(;source=sourcePyrroleWater,
