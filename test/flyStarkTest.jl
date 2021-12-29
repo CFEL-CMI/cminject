2e9cd9d0fddc888914f53b1cf71e338aa745d33e
Timo Borner <timo.borner@cfel.de>
2021-12-29 12:51:40 +0100

    Initial distributions

--- test/flyStarkTest.jl
+++ test/flyStarkTest.jl
@@ -1,17 +1,15 @@
 @testset "Fly Stark Simulation" begin
-    # TODO: Setup from CMIfly and override `unstable_check` to hardcode devices
-    @test 1 == 1
+    # TODO: Read and shift field
     grid = [1e7 2e7; 1e7 2e7]
     itp = CMInject.interpolate(grid, CMInject.BSpline(CMInject.Linear()))
     ext = CMInject.extrapolate(itp, 0)
     itpScaled = CMInject.itpscale(ext, 0:1:1, -0.5:1:0.5)
 
-    # TODO: Normal distribution of velocities and initial positions
-    dists = Dict(:x => CMInject.Dirac(0),
+    dists = Dict(:x => CMInject.Normal(0, 0.0001*0.0001),
                  :y => CMInject.Dirac(0),
-                 :vx => CMInject.Dirac(0),
-                 :vy => CMInject.Dirac(1),
-                 :m => CMInject.Dirac((4*12+7*1.0078+14.003+15.995)/9.223e18)
+                 :vx => CMInject.Normal(0, 1.5*1.5),
+                 :vy => CMInject.Normal(670, 7*7),
+                 :m => CMInject.Dirac((4*12+7*1.0078+14.003+15.995)/9.223e18))
     stateDists = Dict(:J => CMInject.DiscreteUniform(0,0), :M => CMInject.DiscreteUniform(0,0))
     sourcePyrroleWater = CMInject.StarkSamplingSource{CMInject.StarkParticle2D{Float64}, Float64}(
                                      dists, stateDists, "test/pyrrole-water.molecule")
@@ -19,9 +17,8 @@
     @test CMInject.generate(sourcePyrroleWater, 1) != nothing
 
     field = CMInject.ElectricField2D(itpScaled)
-    particles = 5
+    particles = 10
     @inline function detectHits(args...)
-        print("Args: ", args[2].x, ",", args[2].y, "\n")
         x = args[2].x
         y = args[2].y
         # TODO: There are contradictoray information for these values
@@ -36,18 +33,22 @@
         # TODO: Don't ignore deflector hits
         if (y ≥ skimmer1Y-0.01 && y ≤ skimmer1Y &&
             abs(x) ≤ skimmer1R)
+            print("HIT\n")
             return true;
         end
         if (y ≥ skimmer2Y-0.01 && y ≤ skimmer2Y &&
             abs(x) ≤ skimmer2R)
+            print("HIT\n")
             return true;
         end
         if (y ≥ skimmer3Y-0.01 && y ≤ skimmer3Y &&
             abs(x) ≤ skimmer3R)
+            print("HIT\n")
             return true;
         end
         if (y ≥ knifeY-0.01 && y ≤ knifeY &&
             x ≤ knifeX)
+            print("HIT\n")
             return true;
         end
         false
@@ -59,6 +60,7 @@
                                                              -0.001:0.001:0.001,
                                                              true)),
                                             solver=CMInject.EulerHeun(),
+                                            # TODO: Think about reasonable time spans
                                             time_span=(0.0, 1.0),
                                             time_step=1e-1,
                                             ensemble_alg=CMInject.EnsembleThreads(),
@@ -71,8 +73,8 @@
     @test dataPyrroleWater != nothing
 
     for i in 1:particles
-        @test abs(last(dataPyrroleWater[i].u).y + 6.653e-14) < 0.01e-14
-        print(last(dataPyrroleWater[i].u).y, "\n")
+        # TODO: Validate results
+        print(last(dataPyrroleWater[i].u).x, ", ", last(dataPyrroleWater[i].u).y, "\n")
     end
 end
 
