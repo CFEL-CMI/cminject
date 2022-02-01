e3b5b9ce504b416acae5126af488f34a63e536a4
metacolon <timo@teemze.de>
2022-02-01 13:38:07 +0100

    Restrict PT to Stark Particles

--- src/Sources.jl
+++ src/Sources.jl
@@ -8,8 +8,7 @@ struct SamplingSource{PT,Dists<:NamedTuple} <: AbstractSamplingSource where PT<:
 end
 SamplingSource{PT}(dists) where PT = SamplingSource{PT,typeof(dists)}(dists)
 
-# Note that PT should either be StarkParticle or StarkParticle2d
-struct StarkSamplingSource{PT, T} <: AbstractSamplingSource where {PT<:AbstractParticle, T}
+struct StarkSamplingSource{PT, T} <: AbstractSamplingSource where {PT<:Union{StarkParticle,StarkParticle2D}, T}
     distributions::Dict{Symbol, Samp} where
         {Samp<:Sampleable{F, S} where {F<:VariateForm, S<:ValueSupport}}
     # Contains the distributions for the quantum states of the molecules
@@ -30,7 +29,6 @@ function generate(source::StarkSamplingSource{PT}, n::I)::Vector{PT} where {PT, 
     end
     samples = getSamples(source.distributions, n)
     starkCurves = getStarkCurves(source, n)
-    # TODO: Guarantee that PT is a Stark particle
     particles = [PT{typeof(starkCurves[1])}(; starkCurve=starkCurves[i], samples[i]...) for i=1:n]
 end
 
